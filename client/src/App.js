7cc84df9ca6f654eea47b187029d8893293baf53
muha <hnodi.9900@gmail.com>
2022-02-28 19:51:46 +0300

    react init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "./components/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
