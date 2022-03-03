ba0c37747ca42f4fc8a8459343f42240d3aacd24
VEGETA <45381645+VE5ETA@users.noreply.github.com>
2022-03-03 12:05:33 +0300

    fix: token

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,6 @@ const passport = require("passport");
 
 // require("../controllers/LocalStrategy");
 router.post("/signup", Usercontroller.signup);
-console.log("i got here");
 router.post("/login", passport.authenticate("local"), Usercontroller.login);
 router.post("/refreshToken", Usercontroller.refreshToken);
 
