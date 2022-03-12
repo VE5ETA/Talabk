5071bee25d42ca96e32dcb410bf5547cd0a81ff8
VEGETA <45381645+VE5ETA@users.noreply.github.com>
2022-03-12 06:49:20 +0300

    add: Business schema

--- server/models/business.js
+++ server/models/business.js
@@ -2,11 +2,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const business = new Schema({
-  tradeName: String,
-  legalName: String,
-  legalDocument: String,
-  verified: Boolean,
+const BusinessLegalDocs = new Schema({});
+
+const Business = new Schema({
+  ownerID: {
+    // from user collection
+    type: mongoose.SchemaTypes.ObjectId,
+    require: true,
+    ref: "User",
+  },
+  tradeName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  legalName: {
+    type: String,
+    require: true,
+  },
+  businessName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  businessType: {
+    // like restaurant, coffee, ...
+    type: String,
+  },
+  // document: {
+  //   type: Binary,
+  //   require: true,
+  // },
+  businessStatus: {
+    type: Boolean,
+    default: false, // or active
+  },
+
+  businessState: {
+    type: String,
+    default: "suspension", // or active
+  },
+  balance: {
+    type: String,
+  },
 });
 
-module.exports = mongoose.model("business", business);
+module.exports = mongoose.model("Business", Business);
