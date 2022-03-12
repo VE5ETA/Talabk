64ffeeb27c1f53061814906713f435431b4adfd5
muha <muhannadH.1420@gmail.com>
2022-03-12 09:50:05 +0300

    add: business route & controller

--- server/controllers/business.js
+++ server/controllers/business.js
@@ -2,7 +2,14 @@ const business = require("../models/business");
 
 module.exports = {
   create: (req, res, next) => {},
-  read: (req, res, next) => {},
+  info: (req, res, next) => {},
   update: (req, res, next) => {},
   delete: (req, res, next) => {},
+  addEmployee: (req, res, next) => {},
+  removeEmployee: (req, res, next) => {},
+  showNewOrder: (req, res, next) => {},
+  accept: (req, res, next) => {},
+  refusal: (req, res, next) => {},
+  updateStatus: (req, res, next) => {},
+  showActiveOrder: (req, res, next) => {},
 };
--- server/routes/business.js
+++ server/routes/business.js
@@ -3,9 +3,23 @@ const router = require("express").Router();
 const businessController = require("../controllers/business");
 const { verifyUser } = require("../middlewares/authenticate");
 
+// router.get("/read", verifyUser, businessController.read);
+
+// business management
 router.post("/create", verifyUser, businessController.create);
-router.get("/read", verifyUser, businessController.read);
+router.get("/info", verifyUser, businessController.info);
 router.put("/update", verifyUser, businessController.update);
 router.delete("/delete", verifyUser, businessController.delete);
+
+// employees management
+router.post("/addEmployee", verifyUser, businessController.addEmployee);
+router.delete("/removeEmployee", verifyUser, businessController.removeEmployee);
+
+// actions for orders
+router.get("/showNewOrder", verifyUser, businessController.showNewOrder);
+router.post("/accept", verifyUser, businessController.accept);
+router.delete("/refusal", verifyUser, businessController.refusal);
+router.put("/updateStatus", verifyUser, businessController.updateStatus);
+router.get("/showActiveOrder", verifyUser, businessController.showActiveOrder);
 
 module.exports = router;
