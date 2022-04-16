b3cf9bbba48362ee4a1be9c126c27926a937a7fd
muha <muhannadH.1420@gmail.com>
2022-04-16 04:36:17 +0300

    add: platform & customer route

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -2,7 +2,11 @@
 const router = require("express").Router();
 const customerController = require("../controllers/customer");
 
-router.post("/createOrder", customerController.createOrder);
-// router.post("/getOrder", customerController.getOrder);
+router.post("/", customerController.createOrder);
+router.put("/", customerController.editOrder); // only if order not accepted by business
+router.delete("/", customerController.cancelOrder); // only if order not accepted by business
+router.get("/", customerController.getOrder); // get active or current order by phone number
+
+// router.get("/previousOrders", customerController.previousOrders); // get previous order by phone number ( optional )
 
 module.exports = router;
