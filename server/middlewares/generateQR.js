afae0463aa01dc673fb6048db62cbfc76b9d39fc
Mousa ALjohani <45381645+VE5ETA@users.noreply.github.com>
2022-05-12 08:49:58 +0300

    Delete server/middlewares directory

--- server/middlewares/generateQR.js
+++
@@ -1,9 +0,0 @@
-const qrcode = require("qrcode");
-const opts = {
-  errorCorrectionLevel: "H",
-  scale: 50,
-  type: "image/png",
-};
-exports.generateQR = async (username) => {
-  return await qrcode.toDataURL("http://127.0.0.1:8081/@" + username, opts);
-};
