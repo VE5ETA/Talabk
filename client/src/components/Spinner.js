616b2c77ccb159ff4b365b6c32623c29b901a054
VEGETA <45381645+VE5ETA@users.noreply.github.com>
2022-05-31 23:10:56 +0300

    edit: add build No 2 client & customer 🎟

--- client/src/components/Spinner.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-  return <div class="lds-hourglass"></div>;
-}
--- customer/src/context/AuthContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const AuthContext = createContext({
-  isLoggedIn: false,
-  token: null,
-  user: {},
-  // login: () => {},
-  // logout: () => {},
-});
